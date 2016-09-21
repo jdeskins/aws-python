d2e9e9684b893765001369ce52566056534f8426
Jeff Deskins <Jeff.Deskins@lifeway.com>
2016-09-21 10:33:02 -0500

    Improved listing versions from S3

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v $(PWD)/data:/project/data jdeskins/aws-python
+docker run -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION \
+    -v ~/.aws:/root/.aws \
+    -v $(PWD)/data:/project/data \
+    jdeskins/aws-python
--- scripts/list-versions.py
+++ scripts/list-versions.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python
 
+import argparse
 import boto3
-import getopt
-import sys
 
 
-def main(argv):
-    bucket_name = ''
-    prefix = ''
+'''
+Description:
+List versions of objects in an S3 bucket path
 
-    try:
-        opts, args = getopt.getopt(argv,"hb:p:",["bucket_name=", "prefix="])
-    except getopt.GetoptError:
-        print('list-versions.py -b <bucket_name> -p <prefix>')
-        sys.exit(2)
+Example:
+list-versions.py -b <bucket_name> -p <prefix>
+'''
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('list-versions.py -b <bucket_name> -p <prefix>')
-            sys.exit()
-        elif opt in ("-b", "--bucket_name"):
-            bucket_name = arg
-        elif opt in ("-p", "--prefix"):
-            prefix = arg
+
+def main():
+    # Get args passed into the script
+    parser = argparse.ArgumentParser(description='List all versions of objects in a bucket, filtered by prefix.')
+    parser.add_argument('-b','--bucket', help='bucket name', required=True)
+    parser.add_argument('-p','--prefix',help='path within bucket to filter objects', required=False)
+    args = parser.parse_args()
+
+    bucket_name = args.bucket
+    prefix = args.prefix or ''
 
     print('bucket_name=%s' % bucket_name)
     print('prefix=%s' % prefix)
 
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
+    total_mb = 0
 
     versions = bucket.object_versions.filter(Prefix=prefix)
     for obj in versions:
-        print('%s|%s|%s' % (obj.key, obj.last_modified, obj.version_id))
+        if obj.size:
+            size = float(obj.size)/1024/1024
+        else:
+            size = 0
+        total_mb += size
+        print('%s|%s|%s|%sMB' % (obj.key, obj.last_modified, obj.version_id, size))
 
+    print('==================')
+
+    if total_mb > 1024:
+        print('Total: %.1f GB' % (total_mb/1024))
+    else:
+        print('Total: %.1f MB' % total_mb)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
